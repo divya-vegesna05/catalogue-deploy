4b50491f11576f98994e558893f7070e01da6546
divya <divya.vegesna55@gmail.com>
2024-05-14 09:07:49 +0530

    finl

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "catalogue" {
-  name     = "${var.Project_name}-${var.Environment}-${var.tags.Component}"
+  name_prefix    = "${var.Project_name}-${var.Environment}-${var.tags.Component}"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
